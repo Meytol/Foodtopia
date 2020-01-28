73048ad72bcd207d69c403eef9569fa507b308a7
MohammadMahdi Hamzeh <MohammadMahdi.hamzeh@yahoo.com>
2020-01-28 19:16:15 +0330

    +

---
+++ DataAccess/Common/IAuditable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Common
+{
+    public interface IAuditable
+    {
+        int Id { get; set; }
+
+        int CreatedBy { get; set; }
+        DateTime CreatedOn { get; set; }
+
+        int UpdatedBy { get; set; }
+        DateTime? UpdatedOn { get; set; }
+
+        int DeletedBy { get; set; }
+        DateTime? DeletedOn { get; set; }
+
+        int OwnerId { get; set; }
+
+        bool IsDeleted { get; set; }
+
+        #region Relations
+
+       
+
+        #endregion
+    }
+}
