ee26ceb6494bae41f77a675958d1c32f008fe6fd
MohammadMahdi Hamzeh <MohammadMahdi.hamzeh@yahoo.com>
2020-02-01 18:55:30 +0330

    +

---
+++ Service/Repository/CartService.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Context;
+using DataAccess.Model;
+using DataAccess.Repository;
+using Service.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Repository
+{
+    public class CartService : GenericRepository<Cart> ,ICartService
+    {
+        public CartService(DatabaseContext _context)
+            :base(_context)
+        {
+
+        }
+    }
+}
---
+++ Service/Repository/IRepository/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Model;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Repository.IRepository
+{
+    interface ICartService : IGenericRepository<Cart>
+    {
+    }
+}
